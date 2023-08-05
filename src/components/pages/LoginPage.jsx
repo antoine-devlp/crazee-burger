181aa81cea4fa4a200e0aa142ccd757513ef7ecc
Antoine <antoined.contact@gmail.com>
2023-08-05 19:53:13 +0200

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  // state
+  const [login, setLogin] = useState("");
+  // comportement
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Bonjour ${login}`);
+    setLogin("");
+  };
+
+  const handleChange = (event) => {
+    // recup state
+    setLogin(event.target.value);
+  };
+  // render
+  return (
+    <div>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          type="text"
+          value={login}
+          placeholder="Entrez votre prénom..."
+          required
+        />
+        <button>Accédez à votre espace</button>
+      </form>
+    </div>
+  );
 };
