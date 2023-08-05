e8238d8b3edd3969040ecaf79b2f67ee4a56e21c
Antoine <antoined.contact@gmail.com>
2023-08-05 22:59:59 +0200

    🐛 F02: ERROR > test resolving error on vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { LoginPage } from "./components/pages/login/LoginPage";
-
-import "./App.css";
-import { Route, Routes } from "react-router";
 import { OrderPage } from "./components/pages/order/OrderPage";
 import { ErrorPage } from "./components/pages/error/ErrorPage";
+import { Route, Routes } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
     <Routes>
+      <Route path="*" element={<ErrorPage />} />
       <Route path="/" element={<LoginPage />} />
       <Route path="/order/:username" element={<OrderPage />} />
-      <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
 }
--- src/components/pages/error/ErrorPage.jsx
+++ src/components/pages/error/ErrorPage.jsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 
 export const ErrorPage = () => {
   return (
     <div id="error-page">
       <h1>ErrorPage</h1>
       <Link to="/">
-        <button>Retourner à la page d'accueil</button>
+        <button>Retourner à la page d accueil</button>
       </Link>
     </div>
   );
