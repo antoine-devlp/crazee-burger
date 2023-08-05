473b2745e0cc30cb73a4b58e67f4662163596fb9
Antoine <antoined.contact@gmail.com>
2023-08-05 20:01:44 +0200

    ♻️ F01: LOGIN > Create LoginForm

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export const LoginPage = () => {
+export const LoginForm = () => {
   // state
   const [login, setLogin] = useState("");
   // comportement
@@ -16,20 +16,18 @@ export const LoginPage = () => {
   };
   // render
   return (
-    <div>
+    <form action="submit" onSubmit={handleSubmit}>
       <h1>Bienvenue chez nous !</h1>
       <br />
       <h2>Connectez-vous</h2>
-      <form action="submit" onSubmit={handleSubmit}>
-        <input
-          onChange={handleChange}
-          type="text"
-          value={login}
-          placeholder="Entrez votre prénom..."
-          required
-        />
-        <button>Accédez à votre espace</button>
-      </form>
-    </div>
+      <input
+        onChange={handleChange}
+        type="text"
+        value={login}
+        placeholder="Entrez votre prénom..."
+        required
+      />
+      <button>Accédez à votre espace</button>
+    </form>
   );
 };
