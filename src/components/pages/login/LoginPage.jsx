0e8a1ce2b3d79f7c187ab8cac37cccb693fbe35b
Antoine <antoined.contact@gmail.com>
2023-08-06 23:45:05 +0200

    💄:F03 Logo > styling

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -17,18 +17,8 @@ const LoginPageStyled = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  ::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    background: url(${Background}) rgba(0, 0, 0, 0.75);
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-blend-mode: darken;
-    z-index: -1;
-  }
+  background: url(${Background}) rgba(0, 0, 0, 0.75);
+  background-size: cover;
+  background-position: center;
+  background-blend-mode: darken;
 `;
--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -7,26 +7,39 @@ import logo from "/images/logo-orange.png";
 export const Logo = () => {
   return (
     <LogoStyled>
-      CRAZEE <img src={logo} alt="" /> BURGER
+      <h1>CRAZEE</h1>
+      <img src={logo} alt="" />
+      <h1>BURGER</h1>
     </LogoStyled>
   );
 };
 
 const LogoStyled = styled.div`
-  color: ${theme.colors.primary};
-  font-family: ${theme.family.Amatic};
-  font-size: ${theme.fonts.P6};
-  font-weight: ${theme.weights.bold};
   display: flex;
   align-items: center;
-  img {
-    width: 150px;
-  }
   margin-bottom: 40px;
+  scale: 1.5;
+
+  h1 {
+    display: inline;
+    text-align: center;
+    color: ${theme.colors.primary};
+    font-family: ${theme.family.Amatic};
+    font-size: ${theme.fonts.P4};
+    line-height: 1em;
+    font-weight: ${theme.weights.bold};
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+  }
+
+  img {
+    object-fit: contain;
+    object-position: center;
+    height: 60px;
+    width: 80px;
+    margin: 0 5px;
+  }
   @media (min-width: 768px) {
-    font-size: 110px;
-    img {
-      width: 200px;
-    }
+    scale: 2.5;
   }
 `;
