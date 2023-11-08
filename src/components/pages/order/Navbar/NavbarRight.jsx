476e9c2dd23c64e8420c42a85870439f8df06dcf
Antoine <antoined.contact@gmail.com>
2023-11-08 16:54:45 +0100

    🎨 F06 > change format of code to make clean

--- src/components/pages/order/Navbar/NavbarRight.jsx
+++ src/components/pages/order/Navbar/NavbarRight.jsx
@@ -1,52 +1,46 @@
-import { BsPersonCircle } from "react-icons/bs"
-import { Link } from "react-router-dom"
+import { BsPersonCircle } from "react-icons/bs";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { theme } from "../../../../theme";
-import "react-toastify/dist/ReactToastify.css"
+import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { toast } from "react-toastify";
 import ToggleButton from "../../../reusable-ui/ToggleButton";
 import { useState } from "react";
-import {FaUserSecret} from "react-icons/fa6"
+import { FaUserSecret } from "react-icons/fa6";
+import Profile from "./Profile";
 
-export const NavbarRight = ({username}) => {
+export const NavbarRight = ({ username }) => {
   const [isModeAdmin, setIsModeAdmin] = useState(false);
 
-  const notification = () => { 
+  const notification = () => {
     setIsModeAdmin(!isModeAdmin);
-    if (!isModeAdmin){
-    toast.info("Mode admin activé", {
-      icon: <FaUserSecret size={30} />,
-      theme: "dark",
-      position: "bottom-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    })
-  }
-   }
+    if (!isModeAdmin) {
+      toast.info("Mode admin activé", {
+        icon: <FaUserSecret size={30} />,
+        theme: "dark",
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
   return (
     <NavbarRightStyled className="InfosProfil">
-        <ToggleButton labelIfChecked={"DÉSACTIVER LE MODE ADMIN"} labelIfUnchecked={"ACTIVER LE MODE ADMIN"} onToggle={notification}/>
-        <ToastContainer className="toaster" bodyClassName="body-toast"/> {/* TODO: component particulier */}
-        {/* TODO: component particulier */}
-          <div className="profile">
-            <p>
-              Hey, <span className="orange">{username}</span>
-            </p>
-            <Link to="/" className="logout">
-              Se déconnecter
-            </Link>
-          </div>
-          <div className="icon">
-            <BsPersonCircle className="icon" />
-          </div>
-      </NavbarRightStyled>
-  )
-}
+      <ToggleButton
+        labelIfChecked={"DÉSACTIVER LE MODE ADMIN"}
+        labelIfUnchecked={"ACTIVER LE MODE ADMIN"}
+        onToggle={notification}
+      />
+      <ToastContainer className="toaster" bodyClassName="body-toast" />
+      <Profile username={username} />
+    </NavbarRightStyled>
+  );
+};
 
 const NavbarRightStyled = styled.div`
   display: flex;
@@ -73,34 +67,5 @@ const NavbarRightStyled = styled.div`
     div {
       line-height: 1.3em;
     }
-  }
-  .profile {
-    margin-left: 60px;
-    p,
-    .logout {
-      font-family: ${theme.family.Open};
-      font-weight: ${theme.weights.regular};
-    }
-    p {
-      font-size: 16px;
-      margin: 0px;
-    }
-    .orange {
-      color: ${theme.colors.primary};
-    }
-    .logout {
-      color: ${theme.colors.greyBlue};
-      font-size: 10px;
-      text-decoration: none;
-      margin-top: 4px;
-      &:hover {
-        text-decoration: underline;
-        text-underline-offset: 3px;
-      }
-    }
-  }
-  .icon {
-    font-size: 36px;
-    margin-left: 10px;
   }
 `;
